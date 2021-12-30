1a693846ecf7405232e9b3b1f5a7578cbb61429c
jesusb2b <39941574+berabappaditya@users.noreply.github.com>
2021-12-30 10:59:47 +0530

    recursion

--- pepcoding/printDicrRecursion.java
+++ pepcoding/printDicrRecursion.java
@@ -1,14 +1,16 @@
-Public class printDicrRecursion {
+public class printDicrRecursion {
     public static void main(String[] args) {
         int n = 5;
-        printDicrRecursion(n);
+        printDcrRecursion(n);
     }
 
-    public static void printDicrRecursion(int n) {
-        if (n == 0) {
+    public static void printDcrRecursion(int n) {
+        System.out.println("br " + n);
+        if (n < 0) {
             return;
         }
-        System.out.println(n);
-        printDicrRecursion(n - 1);
+        System.out.println("d" + n);
+        printDcrRecursion(n - 1);
+        System.out.println("i" + n);
     }
 }
