d1b009925ecbc13e8cec2e0647c84132aa83f8c4
jesusb2b <39941574+berabappaditya@users.noreply.github.com>
2022-02-05 09:46:59 +0530

    DP Stair case

--- pepcoding/Dynamic_programming/stairClimb/Memo.java
+++ pepcoding/Dynamic_programming/stairClimb/Memo.java
@@ -5,30 +5,32 @@ public class Memo {
 
     public static void main(String[] args) throws Exception {
         // write your code here
-        Scanner scn=new Scanner(System.in);
-        int n=scn.nextInt();
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        System.out.println(ClimbStair(n,dp));
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        System.out.println(ClimbStair(n, dp));
     }
-    public static int ClimbStair(int n, int[] dp){
-        if(n==0){
-            return dp[n]=1;
+
+    public static int ClimbStair(int n, int[] dp) {
+        if (n == 0) {
+            return dp[n] = 1;
         }
-        if(dp[n]!=-1){
+        if (dp[n] != -1) {
             return dp[n];
         }
-        if(n-1>=0){
-            count+=ClimbStair(n-1,dp);
+        int count = 0;
+        if (n - 1 >= 0) {
+            count += ClimbStair(n - 1, dp);
         }
-        if(n-2>=0){
-            count+=ClimbStair(n-2,dp);
+        if (n - 2 >= 0) {
+            count += ClimbStair(n - 2, dp);
         }
-        if(n-3>=0){
-            count+=ClimbStair(n-3,dp);
+        if (n - 3 >= 0) {
+            count += ClimbStair(n - 3, dp);
         }
-        
-        return dp[n]=count;
+
+        return dp[n] = count;
     }
 
 }
