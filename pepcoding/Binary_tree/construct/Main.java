497b5d33b7f651617d2702ded631b9017fb2ef52
jesusb2b <39941574+berabappaditya@users.noreply.github.com>
2022-04-08 11:17:02 +0530

    update

--- pepcoding/Binary_tree/construct/Main.java
+++ pepcoding/Binary_tree/construct/Main.java
@@ -1,68 +1,70 @@
 import java.util.*;
-public class Main{
-    public static class pair{
+
+public class Main {
+    public static class pair {
         Node data;
         int state;
-        pair(int data,int state){
-            this.data=data;
-            this.state=state;
+
+        pair(Node data, int state) {
+            this.data = data;
+            this.state = state;
         }
     }
-    public static class Node{
+
+    public static class Node {
         int data;
         Node left;
         Node right;
-        Node(int data){
-            this.data=data;
-            this.left=null;
-            this.right=null;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
 
         }
-        Node(int data,Node left,Node right){
-            this.data=data;
-            this.left=left;
-            this.right=right;
+
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
         }
     }
-    public static void display(){
-        System.out.println("");
 
-    }
-
-    public static void main(String[] args){
-        Integer [] data={50,25,12,null,null,37,30,null,null,null,75,62,n,70,null,null,87,null,null};
-        Node root=new Node(arr[0]);
-        Stack<pair> st=new Stack<>();
-        pair rootPair=new pair(root,1);
+    public static void main(String[] args) {
+        Integer[] arr = { 50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, n, 70, null, null, 87, null, null };
+        Node root = new Node(arr[0]);
+        Stack<pair> st = new Stack<>();
+        pair rootPair = new pair(root, 1);
         st.push(rootPair);
-        int idx=1;
-        while(st.size!=0){
-            pair peekPair=st.peek();
+        int idx = 1;
+        while (st.size() != 0) {
+            pair peekPair = st.peek();
 
-            //joining  to the left hand side of the node
-            if(peekPair.state==1){
+            // joining to the left hand side of the node
+            if (peekPair.state == 1) {
                 peekPair.state++;
-                if(arr[idx]!=null){
-                    
-                    Node leftChild=new Node(arr[idx]);
-                    peekPair.data.left=leftChild; 
-                    st.push(new pair(leftChild,1));
-                 }
-                 idx++;
-                 //joining  to the right child of the node
-            }else if(peekPair.state==2){
-                peekPair.state++;
-                if(arr[idx]!=null){
-                    
-                    Node rightChild=new Node(arr[idx]);
-                    peekPair.data.right=rightChild; 
-                    st.push(new pair(rightChild,1));
-                 }
-                 idx++;
+                if (arr[idx] != null) {
 
-            }else{
+                    Node leftChild = new Node(arr[idx]);
+                    peekPair.data.left = leftChild;
+                    st.push(new pair(leftChild, 1));
+                }
+                idx++;
+                // joining to the right child of the node
+            } else if (peekPair.state == 2) {
+                peekPair.state++;
+                if (arr[idx] != null) {
+
+                    Node rightChild = new Node(arr[idx]);
+                    peekPair.data.right = rightChild;
+                    st.push(new pair(rightChild, 1));
+                }
+                idx++;
+
+            } else {
                 st.pop();
             }
 
+        }
     }
 }
