f4f355c66c5dea03774bae5d0f9e42b3ed29bc2d
jesusb2b <39941574+berabappaditya@users.noreply.github.com>
2022-04-09 09:35:35 +0530

    update

--- pepcoding/Binary_tree/construct/Main.java
+++ pepcoding/Binary_tree/construct/Main.java
@@ -1,27 +1,11 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static class pair {
-        Node data;
-        int state;
-
-        pair(Node data, int state) {
-            this.data = data;
-            this.state = state;
-        }
-    }
-
     public static class Node {
         int data;
         Node left;
         Node right;
-
-        Node(int data) {
-            this.data = data;
-            this.left = null;
-            this.right = null;
-
-        }
 
         Node(int data, Node left, Node right) {
             this.data = data;
@@ -30,25 +14,69 @@ public class Main {
         }
     }
 
-    public static void display(Node root) {
-        if (root == null) {
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static Node construct(Integer[] arr) {
+        Node root = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
+
+        int idx = 0;
+        while (st.size() > 0) {
+            Pair top = st.peek();
+            if (top.state == 1) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.left = new Node(arr[idx], null, null);
+                    Pair lp = new Pair(top.node.left, 1);
+                    st.push(lp);
+                } else {
+                    top.node.left = null;
+                }
+
+                top.state++;
+            } else if (top.state == 2) {
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.right = new Node(arr[idx], null, null);
+                    Pair rp = new Pair(top.node.right, 1);
+                    st.push(rp);
+                } else {
+                    top.node.right = null;
+                }
+
+                top.state++;
+            } else {
+                st.pop();
+            }
+        }
+
+        return root;
+    }
+
+    public static void display(Node node) {
+        if (node == null) {
             return;
         }
-        if (root.left != null) {
-            System.out.print(root.left.data);
-        } else {
-            System.out.print("..");
-        }
-        System.out.print("<=" + root.data + "=>");
-        if (root.right != null) {
-            System.out.print(root.right.data);
-        } else {
-            System.out.print("..");
-        }
-        System.out.println();
-        display(root.left);
-        display(root.right);
 
+        String str = "";
+        str += node.left == null ? "." : node.left.data + "";
+        str += " <- " + node.data + " -> ";
+        str += node.right == null ? "." : node.right.data + "";
+        System.out.println(str);
+
+        display(node.left);
+        display(node.right);
     }
 
     public static int size(Node root) {
@@ -76,16 +104,22 @@ public class Main {
         if (root == null) {
             return 0;
         }
-        int leftHeight = max(root.left);
-        int rightHeight = max(root.right);
-        return leftHeight > rightHeight ? leftHeight : rightHeight;
+        int lt = max(root.left);
+        int rt = max(root.right);
+        if (root.data > lt && root.data > rt) {
+            return root.data;
+        } else if (rt > lt) {
+            return rt;
+        } else {
+            return lt;
+        }
     }
 
     public static int height(Node root) {
         // write your code here
 
         if (root == null) {
-            return 0;
+            return -1;
         }
         int leftHeight = height(root.left);
         int rightHeight = height(root.right);
@@ -96,46 +130,29 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        Integer[] arr = { 50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null,
-                null };
-        Node root = new Node(arr[0]);
-        Stack<pair> st = new Stack<>();
-        pair rootPair = new pair(root, 1);
-        st.push(rootPair);
-        int idx = 1;
-        while (st.size() != 0) {
-            pair peekPair = st.peek();
-
-            // joining to the left hand side of the node
-            if (peekPair.state == 1) {
-                peekPair.state++;
-                if (arr[idx] != null) {
-
-                    Node leftChild = new Node(arr[idx]);
-                    peekPair.data.left = leftChild;
-                    st.push(new pair(leftChild, 1));
-                }
-                idx++;
-                // joining to the right child of the node
-            } else if (peekPair.state == 2) {
-                peekPair.state++;
-                if (arr[idx] != null) {
-
-                    Node rightChild = new Node(arr[idx]);
-                    peekPair.data.right = rightChild;
-                    st.push(new pair(rightChild, 1));
-                }
-                idx++;
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Integer[] arr = new Integer[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            if (values[i].equals("n") == false) {
+                arr[i] = Integer.parseInt(values[i]);
             } else {
-                st.pop();
+                arr[i] = null;
             }
-
         }
 
-        display(root);
-        System.out.println("height=>" + height(root));
-        System.out.println("Size=>" + size(root));
+        Node root = construct(arr);
+
+        int size = size(root);
+        int sum = sum(root);
+        int max = max(root);
+        int ht = height(root);
+        System.out.println(size);
+        System.out.println(sum);
+        System.out.println(max);
+        System.out.println(ht);
     }
+
 }
